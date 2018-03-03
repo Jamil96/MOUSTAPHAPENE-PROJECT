48a48789f802eb6fdf1ce9ef65761f2f3647488d
jamil96 <moustaphapene96@gmail.com>
2018-03-03 10:10:56 +0000

    Laravel

---
+++ myproject/resources/views/Annonce.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head> 
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Moustapha Pene">
+
+  <title>Annonce</title>
+
+   <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet">
+
+<link href="css/full-width-pics.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+  <header>
+  <nav id="Menu" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">My C@mpus</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul id="menu" class="navbar-nav ml-auto">
+            <li class="nav-item ">
+              <a class="nav-link" href="accueil">Accueil
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="codification">Codification</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="welcome">Laravel</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="Annonce">Annonces</a>
+              <ul>
+                <li><a href="#">Avis de pertes</a></li>
+                <li><a href="#">Avis de ventes</a></li>
+                <li><a href="#">Conference</a></li>
+                <li><a href="#">Recherche de colocataire</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+ </header >
+ <br>
+ <section>
+<fieldset>
+  <h6 > Veuillez  renseigner les informations suivantes</h6>
+<form method="POST" action="traitement.php"  align="center" >
+  <table background-color: gray;>
+      <p>
+      <tr><td><label for="identifiant">
+          Nom d'utilisateur
+        </label></td>
+        <td><input type="text" name="id" id="identifiant"/></td></tr>
+        <tr><td><label for="password">
+          Mot de passe
+        </label></td><td>
+        <input type="text" name="id" id="password"/></td></tr>
+      </p>
+ 
+    <br><br>
+  
+  <tr><td>Publier une annonce</td><td><input type="submit" name="creer" value="Enregistrer"></td></tr>
+  </table>
+</fieldset>
+</section>
+</form>
+</body>
+</html>
---
+++ myproject/resources/views/codification.php
@@ -0,0 +1,83 @@
+<!Doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Moustapha Pene">
+
+	<title>Codification</title>
+
+	 <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet">
+
+<link href="css/full-width-pics.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="cssstyle.css">
+</head>
+<body>
+	<header>	
+	 <nav id="Menu" class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">My C@mpus</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul id="menu" class="navbar-nav ml-auto">
+            <li class="nav-item ">
+              <a class="nav-link" href="accueil.blade.php">Accueil
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="codification.php">Codification</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Restaurant</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Annonce.php">Annonces</a>
+              <ul>
+              	<li><a href="#">Avis de pertes</a></li>
+              	<li><a href="#">Avis de ventes</a></li>
+              	<li><a href="#">Conference</a></li>
+              	<li><a href="#">Recherche de colocataire</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+   </header>
+     <section class="py-5">
+      <div class="container">
+        <h1 text color="red">Codification</h1>
+        <p class="lead">
+        	<p>Le Journal Universitaire partage ce communiqué portant sur les dates de démarrage de la Codification COUD 2017 à l’UCAD. Le communiqué nous a été transmis par la Direction du COUD.</p>
+
+<p>La Direction du COUD informe la communauté universitaire que la commission technique chargée de l’affectation des lits a achevé les travaux de pré-codification le mercredi 22 Mars 2017.</p>
+
+<p>Tout en félicitant l’ensemble des membres de la commission technique, la Direction du COUD a retenu la date du <strong>Lundi 03 Avril 2017 </strong> prochain pour le démarrage de la codification.</p>
+
+Les étudiants admis à la première session et bénéficiaires des œuvres sont invités à se rapprocher des services de l’hébergement du COUD.<br>
+
+<strong>LA SATISFACTION DE L’ETUDIANT, NOTRE CŒUR DE MÉTIER !</strong><br><br><br>
+
+<label ><h5>Le DIRECTEUR du coud</h5>
+
+<h5>CHEIKH OUMAR ANNE</h5></label>
+
+
+        
+      </div>
+    </section>
+
+
+ <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
+
+</body>
+</html>
